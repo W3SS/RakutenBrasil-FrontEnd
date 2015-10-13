b3529d701c797107b5350a9c0963ffaa58fea57f
Igor Vieira Marcondes <igormarcondes@gmail.com>
2015-10-12 22:37:43 -0300

    botstrap 4 migration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,60 @@
 module.exports = function(grunt) {
-	 
+     'use strict';
+
 	  // Plugins do Grunt
 	 grunt.loadNpmTasks('grunt-contrib-uglify');
 	 grunt.loadNpmTasks('grunt-contrib-sass');
 	 grunt.loadNpmTasks('grunt-contrib-watch');
 
 	 grunt.initConfig({
-	 	uglify: {
-	        'home/assets/js-min/rakuten-global.min.js': 'home/assets/js/rakuten-global.js',
-     	}, // uglify
+	 	pkg: grunt.file.readJSON('package.json'),
+	 	
+	 	 // Project paths
+        paths: {
+            app:    'home/',
+            assets: 'assets/',
+            build: {
+                dev: '.dev/',
+                prod: 'dist/'
+            },
+            sass:   'home/assets/sass/',
+            img:    'home/assets/images/',
+            css:    'home/assets/css/',
+            js:     'home/assets/js/',
+            jsmin:  'home/assets/jsmin/'
+        },
 
+        // Uglify: makes javascript ugly. Machines like this way
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: '<%= paths.js %>rakuten-global.js',
+				dest: '<%= paths.jsmin %>rakuten-global.min.js'
+			}
+		},
+
+		// Sass: compiles SCSS files
 		sass : {
 		  dist : {
-			options : { style : 'compressed' },
+			options : { style : 'compressed', sourcemap: 'none' },
 			files : {
-			  'home/assets/css/rakuten-global.css' : 'home/assets/sass/rakuten-global.scss',
-			  'home/assets/css/small-header.css' : 'home/assets/sass/small-header.scss',
-			  'home/assets/css/checkout.min.css' : 'home/assets/sass/checkout.scss'
+			  '<%= paths.css %>rakuten-global.css' : '<%= paths.sass %>rakuten-global.scss',
+			  '<%= paths.css %>small-header.css' : '<%= paths.sass %>small-header.scss',
+			  '<%= paths.css %>checkout.min.css' : '<%= paths.sass %>checkout.scss',
+			  '<%= paths.css %>product.min.css' : '<%= paths.sass %>product.scss',
 			}
 		  }
 		}, // sass
 
 		watch : {
 	      	css: {
-                files: 'home/assets/sass/**/*',
+                files: '<%= paths.sass %>**/*',
                 tasks : ['sass']
         	},
 	    	js: {
-	            files: 'home/assets/js/**/*',
+	            files: '<%= paths.js %>**/*',
 	            tasks: ['uglify']
 	    	}
 	    } // watch		
